3c65d57d4846e57a2ac041fec37498ff1af7b50d
jaimezzapata <zapataval2304@gmail.com>
2024-04-01 16:42:21 -0500

    Se crea estado para los inputs del formulario

--- src/components/layouts/auth/Login.jsx
+++ src/components/layouts/auth/Login.jsx
@@ -1,20 +1,45 @@
-import userLogo from '../../../assets/user.avif'
+import userLogo from "../../../assets/user.avif";
+import { useState } from "react";
+
 const Login = () => {
+  const [getUsuario, setUsuario] = useState("");
+  const [getContrasena, setContrasena] = useState("");
+  const [getCorreo, setCorreo] = useState("");
+  console.log(getUsuario);
   return (
     <form className="login-form">
       <img src={userLogo} alt="Usuario" />
       <h2>Iniciar sesión</h2>
       <div className="input-group">
         <label htmlFor="username">Usuario:</label>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsuario(e.target.value);
+          }}
+        />
       </div>
       <div className="input-group">
         <label htmlFor="password">Contraseña:</label>
-        <input type="text" name="" id="" />
+        <input
+          onChange={(e) => {
+            setContrasena(e.target.value);
+          }}
+          type="text"
+          name=""
+          id=""
+        />
       </div>
       <div className="input-group">
         <label htmlFor="email">Correo electrónico:</label>
-        <input type="text" name="" id="" />
+        <input
+          onChange={(e) => {
+            setCorreo(e.target.value);
+          }}
+          type="text"
+          name=""
+          id=""
+        />
       </div>
       <button type="submit">Iniciar sesión</button>
     </form>
